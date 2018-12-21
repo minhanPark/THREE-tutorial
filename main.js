d9d2578087c953fbcc065a941340d379adabf177
minhanPark <alsgks1218@naver.com>
2018-12-21 16:02:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const Init = () => {
     HEIGHT = window.innerHeight;
 
   const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x81ecec);
 
   const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000);
   scene.add(camera);
@@ -18,12 +19,16 @@ const Init = () => {
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(WIDTH, HEIGHT);
-  renderer.setClearColor(0x81ecec);
+  //renderer.setClearColor(0x81ecec);
   document.body.appendChild(renderer.domElement);
 
   camera.position.z = 30;
   mesh.rotation.x = (Math.PI * 20) / 180;
   mesh.rotation.y = (Math.PI * 20) / 180;
+  
+  //if you want to drag mesh(obj), use bottom line code.
+  // const obj = [mesh];
+  // const dragControls = new THREE.DragControls(obj, camera, renderer.domElement);
 
   const rotateObj = () => {
     const speed = Math.floor(Math.random() * 3);
